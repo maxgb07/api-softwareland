aa7b90bb332ae55a272e383d93ddd66a0de556a8
maxgb07 <39058291+maxgb07@users.noreply.github.com>
2021-10-20 14:53:18 -0500

    Update server.js
    cambio host db

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.connect('mongodb://189.243.218.84/Tododb'); 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
